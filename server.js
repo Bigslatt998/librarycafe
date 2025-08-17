6660e0985508dc82b78e41e2f1c84b2399f978c8
BigSlatt998 <ganiyutaofeeqolarewaju@gmail.com>
2025-08-17 21:59:41 +0100

    All

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: ['http://localhost:5173',
       'https://bigslatt998.github.io',
-      'https://bigslatt998.github.io/librarycafe'],
+      'https://bigslatt998.github.io/librarycafe',
+      'https://librarycafe-api.onrender.com',
+      'https://librarycafe-csuo.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
